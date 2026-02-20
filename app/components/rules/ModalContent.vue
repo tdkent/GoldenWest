289657a65e04d83cd931fa529f0c9b04630b007f
Tim <timkentdev@gmail.com>
2026-02-20 15:38:43 -0800

    update card modal content

--- app/components/rules/ModalContent.vue
+++ app/components/rules/ModalContent.vue
@@ -2,16 +2,16 @@
 	<div class="flex flex-col items-center justify-center gap-4">
 		<div
 			class="relative flex aspect-[5/3] w-4/5 flex-col items-center justify-center gap-4 border border-gray-400 font-splash shadow-[4px_4px_1px_0_rgba(0,0,0,0.2)] dark:shadow-gray-100">
-			<span class="block italic"><q>Horse Name</q></span>
-			<span>Entrant's Name</span>
-			<span class="absolute bottom-1 right-2 text-xs">Side A</span>
+			<span class="block">Class Number and Class Name</span>
+			<span>Breed</span>
+			<span>Gender</span>
+			<span class="absolute bottom-1 right-2 text-xs">Front</span>
 		</div>
 		<div
 			class="relative flex aspect-[5/3] w-4/5 flex-col items-center justify-center gap-4 border border-gray-400 font-splash shadow-[4px_4px_1px_0_rgba(0,0,0,0.2)] dark:shadow-gray-100">
-			<span class="block">Class &num;</span>
-			<span class="block">Breed</span>
-			<span>Gender</span>
-			<span class="absolute bottom-1 right-2 text-xs">Side B</span>
+			<span class="block">Exhibitor Name</span>
+			<span class="block">"Model Name"</span>
+			<span class="absolute bottom-1 right-2 text-xs">Back</span>
 		</div>
 	</div>
 </template>
