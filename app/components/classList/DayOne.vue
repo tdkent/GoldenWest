739cfd7d3c9845fce470575ecaedd66adb9a55c6
Tim <timkentdev@gmail.com>
2026-02-22 15:08:05 -0800

    add basic day one class list component with fetch

---
+++ app/components/classList/DayOne.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { API_URL } from '~/constants';
+import type { Division } from '~/types/class-list.index';
+const { data, pending, error } = await useFetch<Division[]>(`${API_URL}/db_day1.json`, {
+  server: false,
+})
+</script>
+
+<template>
+  <div v-if="pending">Loading...</div>
+  <div v-else-if="error">Error loading data</div>
+  <div v-else>
+    <!-- display data -->
+  </div>
+</template>
