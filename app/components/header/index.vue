5bb0dbb60d3b3c8c6221e5a0e1390e50361d1d3d
Tim <timkentdev@gmail.com>
2026-02-26 12:31:53 -0800

    Fix: revert splash html (#20)

--- app/components/header/index.vue
+++ app/components/header/index.vue
@@ -37,22 +37,24 @@
 </script>
 
 <template>
-	<header
-		id="header"
-		class="mx-auto max-w-screen-xl border-b">
-		<div class="flex flex-col pt-2 md:px-8 lg:pt-4 xl:px-4">
-			<div class="flex items-center justify-between">
-				<HeaderNavMobile
-					v-if="viewport.isLessThan('lg')"
-					:navLinks="navLinks" />
-				<HeaderText />
-				<HeaderDarkModeSwitch v-if="viewport.isLessThan('lg')" />
-			</div>
-			<div
-				v-if="viewport.isGreaterOrEquals('lg')"
-				class="flex justify-between">
-				<HeaderNavDesktop :navLinks="navLinks" />
-				<HeaderDarkModeSwitch />
+	<header id="header">
+		<div class="border-b">
+			<div class="mx-auto max-w-screen-xl">
+				<div class="flex flex-col pt-2 md:px-8 lg:pt-4 xl:px-4">
+					<div class="flex items-center justify-between">
+						<HeaderNavMobile
+							v-if="viewport.isLessThan('lg')"
+							:navLinks="navLinks" />
+						<HeaderText />
+						<HeaderDarkModeSwitch v-if="viewport.isLessThan('lg')" />
+					</div>
+					<div
+						v-if="viewport.isGreaterOrEquals('lg')"
+						class="flex justify-between">
+						<HeaderNavDesktop :navLinks="navLinks" />
+						<HeaderDarkModeSwitch />
+					</div>
+				</div>
 			</div>
 		</div>
 		<HeaderSplash />
