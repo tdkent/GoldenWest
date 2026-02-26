3e0720252471c05432654b48d4ceaa709ed99ebf
Tim <timkentdev@gmail.com>
2026-02-25 21:44:52 -0800

    add nuxt image pkg

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,32 +1,33 @@
 export default defineNuxtConfig({
-	compatibilityDate: '2024-04-03',
-	css: ["~/assets/css/tailwind.css"],
-	devtools: { enabled: false },
-	modules: [
-        '@nuxt/ui',
-        '@nuxtjs/tailwindcss',
-        "@nuxt/scripts",
-        '@nuxtjs/color-mode',
-        "nuxt-viewport"
-    ],
-		viewport: {
-			breakpoints: {
-				xs: 360,
-				sm: 480,
-				md: 648,
-				lg: 1026,
-				xl: 1440,
-			},
-			defaultBreakpoints: {
-				xs: 'xs',
-				sm: 'sm',
-				md: 'md',
-				lg: 'lg',
-				xl: 'xl'
-			},
-			fallbackBreakpoint: 'lg'
-		},
-	colorMode: {
-		preference: 'system'
-	},
+				compatibilityDate: '2024-04-03',
+				css: ["~/assets/css/tailwind.css"],
+				devtools: { enabled: false },
+				modules: [
+				 '@nuxt/ui',
+				 '@nuxtjs/tailwindcss',
+				 "@nuxt/scripts",
+				 '@nuxtjs/color-mode',
+				 "nuxt-viewport",
+				 '@nuxt/image',
+				],
+								viewport: {
+												breakpoints: {
+																xs: 360,
+																sm: 480,
+																md: 648,
+																lg: 1026,
+																xl: 1440,
+												},
+												defaultBreakpoints: {
+																xs: 'xs',
+																sm: 'sm',
+																md: 'md',
+																lg: 'lg',
+																xl: 'xl'
+												},
+												fallbackBreakpoint: 'lg'
+								},
+				colorMode: {
+								preference: 'system'
+				},
 })
