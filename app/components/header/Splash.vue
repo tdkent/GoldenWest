94fb06fc2d3fdc3be296f9559536f86062581543
Tim <timkentdev@gmail.com>
2026-02-27 08:57:23 -0800

    revert NuxtImg to standard img element

--- app/components/header/Splash.vue
+++ app/components/header/Splash.vue
@@ -5,33 +5,25 @@
 </script>
 
 <template>
-	<div
+		<div
 		v-if="route.path === '/'"
 		class="relative flex aspect-[7/5] max-h-[500px] w-full items-center justify-center overflow-hidden md:h-auto md:max-h-[500px] lg:justify-end">
-		<NuxtImg
+		<img
 			:src="`${imgBaseUrl}/model-horse-1.webp`"
-			fill
 			alt="Model of a horse facing left"
-			class="splash-slide animate-slide-1 md:object-[0%_20%] lg:object-[0%_14%]"
-			sizes="100vw" />
-		<NuxtImg
+			class="splash-slide animate-slide-1 md:object-[0%_20%] lg:object-[0%_14%]" />
+		<img
 			:src="`${imgBaseUrl}/model-horse-4.webp`"
-			fill
 			alt="Model horse facing right"
-			class="splash-slide animate-slide-2 md:object-[0%_30%] lg:object-[0%_20%]"
-			sizes="100vw" />
-		<NuxtImg
+			class="splash-slide animate-slide-2 md:object-[0%_30%] lg:object-[0%_20%]" />
+		<img
 			:src="`${imgBaseUrl}/model-horse-3.webp`"
-			fill
 			alt="Model of a horse facing left"
-			class="splash-slide animate-slide-3 md:object-[0%_5%]"
-			sizes="100vw" />
-		<NuxtImg
+			class="splash-slide animate-slide-3 md:object-[0%_5%]" />
+		<img
 			:src="`${imgBaseUrl}/model-horse-2.webp`"
-			fill
 			alt="Model of a horse and rider"
-			class="splash-slide animate-slide-4 md:object-[0%_20%]"
-			sizes="100vw" />
+			class="splash-slide animate-slide-4 md:object-[0%_20%]" />
 		<div
 			class="absolute h-full w-full bg-[linear-gradient(to_right,rgba(222,145,29,0.9),rgba(247,201,72,0.7),rgba(247,201,72,0.8),rgba(64,195,247,0.7))]"></div>
 		<HeaderTaglines />
