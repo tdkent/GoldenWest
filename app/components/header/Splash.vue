ee1396e5387c849331f1cfca87f44ab569a1d111
Tim <timkentdev@gmail.com>
2026-02-28 08:53:29 -0800

    fix: remove fill attribute on img tags

--- app/components/header/Splash.vue
+++ app/components/header/Splash.vue
@@ -10,22 +10,18 @@
 		class="relative flex aspect-[7/5] max-h-[500px] w-full items-center justify-center overflow-hidden md:h-auto md:max-h-[500px] lg:justify-end">
 		<img
 			:src="`${imgBaseUrl}/model-horse-1.webp`"
-			fill
 			alt="Model of a horse facing left"
 			class="splash-slide animate-slide-1 md:object-[0%_20%] lg:object-[0%_14%]" />
 		<img
 			:src="`${imgBaseUrl}/model-horse-4.webp`"
-			fill
 			alt="Model horse facing right"
 			class="splash-slide animate-slide-2 md:object-[0%_30%] lg:object-[0%_20%]" />
 		<img
 			:src="`${imgBaseUrl}/model-horse-3.webp`"
-			fill
 			alt="Model of a horse facing left"
 			class="splash-slide animate-slide-3 md:object-[0%_5%]" />
 		<img
 			:src="`${imgBaseUrl}/model-horse-2.webp`"
-			fill
 			alt="Model of a horse and rider"
 			class="splash-slide animate-slide-4 md:object-[0%_20%]" />
 		<div
