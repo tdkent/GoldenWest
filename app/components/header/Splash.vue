e75910e9020687bbb25f47f111f31c7a8506a015
Tim <timkentdev@gmail.com>
2026-03-24 15:39:49 -0700

    Fix: update splash img positions (#42)

--- app/components/header/Splash.vue
+++ app/components/header/Splash.vue
@@ -9,8 +9,7 @@
 	<div
 		v-if="route.path === '/'"
 		class="relative flex aspect-[7/5] max-h-[500px] w-full items-center justify-center overflow-hidden md:h-auto md:max-h-[500px] lg:justify-end">
-		<picture
-			class="splash-slide animate-slide-1 md:object-[0%_20%] lg:object-[0%_14%]">
+		<picture class="splash-slide animate-slide-1">
 			<source
 				:srcset="createSrcSets(1).avif"
 				type="image/avif" />
@@ -18,13 +17,13 @@
 				:srcset="createSrcSets(1).webp"
 				type="image/webp" />
 			<img
+				class="h-full w-full object-cover md:object-[0%_20%] lg:object-[0%_14%]"
 				:src="`${imgBaseUrl}/model-horse-1/model-horse-1@1280.jpeg`"
 				:srcset="createSrcSets(1).jpeg"
 				fetchpriority="high"
 				alt="Model horse" />
 		</picture>
-		<picture
-			class="splash-slide animate-slide-2 md:object-[0%_30%] lg:object-[0%_20%]">
+		<picture class="splash-slide animate-slide-2">
 			<source
 				:srcset="createSrcSets(4).avif"
 				type="image/avif" />
@@ -32,12 +31,13 @@
 				:srcset="createSrcSets(4).webp"
 				type="image/webp" />
 			<img
+				class="h-full w-full object-cover md:object-[0%_30%] lg:object-[0%_40%] xl:object-[0%_30%]"
 				:src="`${imgBaseUrl}/model-horse-4/model-horse-4@1280.jpeg`"
 				:srcset="createSrcSets(4).jpeg"
 				fetchpriority="high"
 				alt="Model horse" />
 		</picture>
-		<picture class="splash-slide animate-slide-3 md:object-[0%_5%]">
+		<picture class="splash-slide animate-slide-3">
 			<source
 				:srcset="createSrcSets(3).avif"
 				type="image/avif" />
@@ -45,12 +45,13 @@
 				:srcset="createSrcSets(3).webp"
 				type="image/webp" />
 			<img
+				class="h-full w-full object-cover md:object-[0%_5%] xl:object-[0%_10%]"
 				:src="`${imgBaseUrl}/model-horse-3/model-horse-3@1280.jpeg`"
 				:srcset="createSrcSets(3).jpeg"
 				fetchpriority="high"
 				alt="Model horse with show ribbons" />
 		</picture>
-		<picture class="splash-slide animate-slide-4 md:object-[0%_20%]">
+		<picture class="splash-slide animate-slide-4">
 			<source
 				:srcset="createSrcSets(2).avif"
 				type="image/avif" />
@@ -58,6 +59,7 @@
 				:srcset="createSrcSets(2).webp"
 				type="image/webp" />
 			<img
+				class="h-full w-full object-cover md:object-[0%_20%]"
 				:src="`${imgBaseUrl}/model-horse-2/model-horse-2@1280.jpeg`"
 				:srcset="createSrcSets(2).jpeg"
 				fetchpriority="high"
